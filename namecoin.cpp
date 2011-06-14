12fdd4def959c94a5515972894c14b6a55fca16d
Khalahan <khal@bitcoin-contact.org>
2011-06-14 10:53:40 +0200

    Fix typo

--- namecoin.cpp
+++ namecoin.cpp
@@ -1443,7 +1443,7 @@ bool CNamecoinHooks::ConnectInputs(CTxDB& txdb,
             if (mapNamePending[vvchArgs[0]].count(tx.GetHash()))
                 mapNamePending[vvchArgs[0]].erase(tx.GetHash());
             else
-                printf("ConnectInputsHook() : connecting inputs on %s which was not in pending - must be someone elses",
+                printf("ConnectInputsHook() : connecting inputs on %s which was not in pending - must be someone elses\n",
                         tx.GetHash().GetHex().c_str());
         }
     }
